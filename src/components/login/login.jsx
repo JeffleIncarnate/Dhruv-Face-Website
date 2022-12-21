10e0452b6a394a14646199bdb040a18d0296e71d
dhruv <dhruvrayat50@gmail.com>
2022-12-22 09:53:04 +1300

    Update: Made the Login page actually a login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -8,43 +8,52 @@ export default function Login() {
   const password = useRef(null);
 
   return (
-    <div className="form_wrapper">
-      <form action="" className="login_form">
-        <h1>Login</h1>
+    <>
+      <img
+        className="nav_img"
+        src="../../../public/Logo.png"
+        alt="Logo of Dhruv-Face"
+      />
+      <div className="form_wrapper">
+        <form action="" className="login_form">
+          <h1>Dhruv Face</h1>
 
-        <label htmlFor="username">
-          Username <span className="asterix">*</span>
-        </label>
-        <input
-          id="username"
-          ref={username}
-          required
-          name="username"
-          placeholder="SuperCoolUsername"
-          type="text"
-        ></input>
+          <label htmlFor="username">
+            Username <span className="asterix">*</span>
+          </label>
+          <input
+            id="username"
+            ref={username}
+            required
+            name="username"
+            placeholder="SuperCoolUsername"
+            type="text"
+          ></input>
 
-        <label htmlFor="password">
-          Password <span className="asterix">*</span>
-        </label>
-        <input
-          id="password"
-          ref={password}
-          required
-          name="username"
-          placeholder="SuperSecurePassword"
-          type="password"
-        ></input>
+          <label htmlFor="password">
+            Password <span className="asterix">*</span>
+          </label>
+          <input
+            id="password"
+            ref={password}
+            required
+            name="username"
+            placeholder="SuperSecurePassword"
+            type="password"
+          ></input>
 
-        <div className="bottom_form">
-          <p>
-            Dont have an account?{" "}
-            <Link className="link" to="register">
-              Register now!
-            </Link>
-          </p>
-        </div>
-      </form>
-    </div>
+          <input type="submit" value="Login" />
+
+          <div className="bottom_form">
+            <p>
+              Dont have an account?{" "}
+              <Link className="link" to="register">
+                Register now!
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
